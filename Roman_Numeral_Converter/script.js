002ec7c156225e984c48d1f5fc4a80d7a52ab51c
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-12 09:22:29 +0200

    Add Roman Numeral Converter to the github.

---
+++ Roman_Numeral_Converter/script.js
@@ -0,0 +1,35 @@
+const numberInput = document.getElementById('number');
+const convertButton = document.getElementById('convert-btn');
+const output = document.getElementById('output');
+
+function convertToRoman() {
+    if (number.value == "") {
+        output.innerHTML = "Please enter a valid number";
+        return;
+    } else if (number.value < 1) {
+        output.innerHTML = "Please enter a number greater than or equal to 1";
+        return;
+    }
+
+    if (number.value > 3999) {
+        output.innerHTML = "Please enter a number less than or equal to 3999";
+        return;
+    }
+
+    const romanSymbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    const romanValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+    let romanNumeral = "";
+
+    for (let i = 0; i < romanValues.length; i++) {
+        while (number.value >= romanValues[i]) {
+            romanNumeral += romanSymbols[i];
+            number.value -= romanValues[i];
+        }
+    }
+
+    output.innerHTML = romanNumeral;
+}
+
+convertButton.addEventListener("click", convertToRoman);
